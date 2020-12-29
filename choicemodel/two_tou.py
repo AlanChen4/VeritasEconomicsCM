d74d79bc1452b40bc1cdb7a7c26366f10e477ab8
Alan Chen <avchen4@gmail.com>
2020-12-29 02:54:28 -0500

    Add plans A and B

--- choicemodel/two_tou.py
+++ choicemodel/two_tou.py
@@ -24,6 +24,116 @@ class TwoTOU(wx.Frame):
 
         plans_sizer.Add(status_quo_sizer)
 
+        # time of use A plan
+        tou_a_sizer = wx.BoxSizer(wx.VERTICAL)
+        tou_a_label = wx.StaticText(self, label='Time-of-Use (TOU) A')
+        tou_a_sizer.Add(tou_a_label, proportion=1, flag=wx.ALL, border=10)
+
+        plans_sizer.Add(tou_a_sizer)
+
+        # off peak price
+        off_peak_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
+        off_peak_label_a = wx.StaticText(self, label='Off-Peak Price')
+        off_peak_sizer_a.Add(off_peak_label_a, proportion=1, flag=wx.ALL, border=10)
+        off_peak_price_a = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        off_peak_sizer_a.Add(off_peak_price_a, proportion=1, flag=wx.ALL, border=10)
+        tou_a_sizer.Add(off_peak_sizer_a)
+
+        # peak price
+        peak_price_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
+        peak_label_a = wx.StaticText(self, label='Peak Price')
+        peak_price_sizer_a.Add(peak_label_a, proportion=1, flag=wx.ALL, border=10)
+        peak_price = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_price_sizer_a.Add(peak_price, proportion=1, flag=wx.ALL, border=10)
+        tou_a_sizer.Add(peak_price_sizer_a)
+
+        # peak period
+        peak_period_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
+        peak_period_label_a = wx.StaticText(self, label='Peak Period')
+        peak_period_sizer_a.Add(peak_period_label_a, proportion=1, flag=wx.ALL, border=10)
+        peak_period_a = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_period_sizer_a.Add(peak_period_a, proportion=1, flag=wx.ALL, border=10)
+        tou_a_sizer.Add(peak_period_sizer_a)
+
+        # peak season
+        peak_season_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
+        peak_season_label_a = wx.StaticText(self, label='Peak Season')
+        peak_season_sizer_a.Add(peak_season_label_a, proportion=1, flag=wx.ALL, border=10)
+        peak_season_a = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_season_sizer_a.Add(peak_season_a, proportion=1, flag=wx.ALL, border=10)
+        tou_a_sizer.Add(peak_season_sizer_a)
+
+        # time of use B plan
+        tou_b_sizer = wx.BoxSizer(wx.VERTICAL)
+        tou_b_label = wx.StaticText(self, label='Time-of-Use (TOU) B')
+        tou_b_sizer.Add(tou_b_label, proportion=1, flag=wx.ALL, border=10)
+
+        plans_sizer.Add(tou_b_sizer, proportion=1, flag=wx.ALL | wx.EXPAND)
+
+        # off peak price
+        off_peak_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
+        off_peak_label_b = wx.StaticText(self, label='Off-Peak Price')
+        off_peak_sizer_b.Add(off_peak_label_b, proportion=1, flag=wx.ALL, border=10)
+        off_peak_price_b = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        off_peak_sizer_b.Add(off_peak_price_b, proportion=1, flag=wx.ALL, border=10)
+        tou_b_sizer.Add(off_peak_sizer_b)
+
+        # peak price
+        peak_price_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
+        peak_label_b = wx.StaticText(self, label='Peak Price')
+        peak_price_sizer_b.Add(peak_label_b, proportion=1, flag=wx.ALL, border=10)
+        peak_price = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_price_sizer_b.Add(peak_price, proportion=1, flag=wx.ALL, border=10)
+        tou_b_sizer.Add(peak_price_sizer_b)
+
+        # peak period
+        peak_period_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
+        peak_period_label_b = wx.StaticText(self, label='Peak Period')
+        peak_period_sizer_b.Add(peak_period_label_b, proportion=1, flag=wx.ALL, border=10)
+        peak_period_b = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_period_sizer_b.Add(peak_period_b, proportion=1, flag=wx.ALL, border=10)
+        tou_b_sizer.Add(peak_period_sizer_b)
+
+        # peak season
+        peak_season_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
+        peak_season_label_b = wx.StaticText(self, label='Peak Season')
+        peak_season_sizer_b.Add(peak_season_label_b, proportion=1, flag=wx.ALL, border=10)
+        peak_season_b = wx.ComboBox(self, value='SELECT', choices=[
+            'Price A',
+            'Price B',
+            'Price C'
+        ])
+        peak_season_sizer_b.Add(peak_season_b, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
+        tou_b_sizer.Add(peak_season_sizer_b)
+
         vbox.Add(plans_sizer)
         self.SetSizer(vbox)
 
