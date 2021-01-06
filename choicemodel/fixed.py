c77d8b97ec8d044f6ac4d3986aa8a301733bf2eb
Alan Chen <avchen4@gmail.com>
2021-01-05 21:00:53 -0500

    Modularize the fixed plan and refactor code

---
+++ choicemodel/fixed.py
@@ -0,0 +1,35 @@
+import wx
+
+
+class Fixed(wx.Panel):
+
+    def __init__(self, plan_title, parent=None):
+        super(Fixed, self).__init__(parent=parent)
+        self.init_ui(plan_title)
+
+    def init_ui(self, plan_title):
+        root_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        fixed_font = wx.Font(wx.FontInfo(10).Bold())
+        fixed_label = wx.StaticText(self, label=f'Fixed Bill {plan_title}')
+        fixed_label.SetFont(fixed_font)
+        root_sizer.Add(fixed_label, flag=wx.ALL, border=10)
+
+        premium_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        premium_label = wx.StaticText(self, size=(150, 20), label='Premium (%)')
+        premium_rate = wx.ComboBox(self, choices=['Some %'])
+
+        premium_sizer.Add(premium_label, flag=wx.ALL, border=10)
+        premium_sizer.Add(premium_rate, flag=wx.ALL, border=10)
+
+        contract_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        contract_label = wx.StaticText(self, size=(150,20), label='Contract Length')
+        contract_length = wx.ComboBox(self, choices=['Some Length'])
+
+        contract_sizer.Add(contract_label, flag=wx.ALL, border=10)
+        contract_sizer.Add(contract_length, flag=wx.ALL, border=10)
+
+        root_sizer.Add(premium_sizer)
+        root_sizer.Add(contract_sizer)
+
+        self.SetSizer(root_sizer)
--- choicemodel/two_fixed.py
+++ choicemodel/two_fixed.py
@@ -1,5 +1,9 @@
 import wx
 
+from .fixed import Fixed
+from .results import Results
+from .status_quo import StatusQuo
+
 
 class TwoFixed(wx.Frame):
 
@@ -11,87 +15,22 @@ class TwoFixed(wx.Frame):
     def init_ui(self):
         vbox = wx.BoxSizer(wx.VERTICAL)
 
-        # plans sizer
+        # Used to store status quo and plans
         plans_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        # status quo
-        status_quo_sizer = wx.BoxSizer(wx.VERTICAL)
-        status_quo_label = wx.StaticText(self, label='Status Quo')
-        status_quo_sizer.Add(status_quo_label, flag=wx.ALL, border=10)
-        status_quo_rate = wx.StaticText(self, label='Status Quo Rate ($/kWh)  $0.10')
-        status_quo_sizer.Add(status_quo_rate, flag=wx.ALL, border=10)
+        # Create Status Quo and Fixed Plans
+        status_quo = StatusQuo(self)
+        fixed_a = Fixed(parent=self, plan_title='A')
+        fixed_b = Fixed(parent=self, plan_title='B')
 
-        # fixed A
-        fixed_sizer_a = wx.BoxSizer(wx.VERTICAL)
-        fixed_label_a = wx.StaticText(self, label='Fixed Bill A')
-        fixed_sizer_a.Add(fixed_label_a, flag=wx.ALL, border=10)
+        results = Results(self)
 
-        premium_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
-        premium_label_a = wx.StaticText(self, label='Premium (%)')
-        premium_rate_a = wx.ComboBox(self, choices=['Some %'])
-        premium_sizer_a.Add(premium_label_a, flag=wx.ALL, border=10)
-        premium_sizer_a.Add(premium_rate_a, flag=wx.ALL, border=10)
-        fixed_sizer_a.Add(premium_sizer_a)
+        plans_sizer.Add(status_quo)
+        plans_sizer.Add(fixed_a)
+        plans_sizer.Add(fixed_b)
 
-        contract_sizer_a = wx.BoxSizer(wx.HORIZONTAL)
-        contract_label_a = wx.StaticText(self, label='Contract Length')
-        contract_length_a = wx.ComboBox(self, choices=['Some Length'])
-        contract_sizer_a.Add(contract_label_a, flag=wx.ALL, border=10)
-        contract_sizer_a.Add(contract_length_a, flag=wx.ALL, border=10)
-        fixed_sizer_a.Add(contract_sizer_a)
-
-        # fixed B
-        fixed_sizer_b = wx.BoxSizer(wx.VERTICAL)
-        fixed_label_b = wx.StaticText(self, label='Fixed Bill B')
-        fixed_sizer_b.Add(fixed_label_b, flag=wx.ALL, border=10)
-
-        premium_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
-        premium_label_b = wx.StaticText(self, label='Premium (%)')
-        premium_rate_b = wx.ComboBox(self, choices=['Some %'])
-        premium_sizer_b.Add(premium_label_b, flag=wx.ALL, border=10)
-        premium_sizer_b.Add(premium_rate_b, flag=wx.ALL, border=10)
-        fixed_sizer_b.Add(premium_sizer_b)
-
-        contract_sizer_b = wx.BoxSizer(wx.HORIZONTAL)
-        contract_label_b = wx.StaticText(self, label='Contract Length')
-        contract_length_b = wx.ComboBox(self, choices=['Some Length'])
-        contract_sizer_b.Add(contract_label_b, flag=wx.ALL, border=10)
-        contract_sizer_b.Add(contract_length_b, flag=wx.ALL, border=10)
-        fixed_sizer_b.Add(contract_sizer_b)
-
-        # Add to plans sizer
-        plans_sizer.Add(status_quo_sizer)
-        plans_sizer.Add(fixed_sizer_a)
-        plans_sizer.Add(fixed_sizer_b)
-
-        # results label
-        results_label = wx.StaticText(self, label='Results - Predictions of Potential Market Size')
-
-        # calculate service
-        calculate_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        calculate_service_label = wx.StaticText(self, label='Potential Market Size')
-        calculate_sizer.Add(calculate_service_label, flag=wx.ALL, border=10)
-
-        # service territory
-        service_sizer = wx.BoxSizer(wx.VERTICAL)
-        service_label = wx.StaticText(self, label='Service Territory')
-        service_button = wx.Button(self, label='Calculate')
-        service_sizer.Add(service_label)
-        service_sizer.Add(service_button, flag=wx.ALL, border=10)
-        calculate_sizer.Add(service_sizer, flag=wx.LEFT, border=20)
-
-        # zip code
-        zip_sizer = wx.BoxSizer(wx.VERTICAL)
-        zip_label = wx.StaticText(self, label='By Zip Code')
-        zip_button = wx.Button(self, label='Calculate')
-        zip_sizer.Add(zip_label)
-        zip_sizer.Add(zip_button, flag=wx.ALL, border=10)
-        calculate_sizer.Add(zip_sizer, flag=wx.LEFT, border=20)
-
-        # Add to main sizer
         vbox.Add(plans_sizer)
-        vbox.Add(results_label, flag=wx.ALL, border=20)
-        vbox.Add(calculate_sizer)
+        vbox.Add(results)
 
         vbox.SetSizeHints(self)
         self.SetSizer(vbox)
