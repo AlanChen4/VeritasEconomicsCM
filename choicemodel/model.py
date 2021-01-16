bbe30a0d7b81a5690451c445e5e833756d561bfc
Alan Chen <avchen4@gmail.com>
2021-01-16 12:47:42 -0500

    Change function into class

--- choicemodel/model.py
+++ choicemodel/model.py
@@ -5,70 +5,72 @@ from .constants import *
 from pathlib import Path
 
 
-def main():
-    tn_data_path = Path(__file__).parent / '../data/TNdata.csv'
-    tn_df = pd.read_csv(tn_data_path)
-    tn_df = tn_df.drop(tn_df.columns[[0]], axis=1)
+class Model:
 
-    # data without 1st column and headers
-    tn_data = tn_df.values
+    def __init__(self):
+        self.p1_peak = np.array([0.34])
+        self.p1_off_peak = np.array([0.08])
+        self.p1_peak_time = BASE['two_to_five']
+        self.p1_peak_season = SEASONS['summer']
 
-    participation_coeff_path = Path(__file__).parent / '../data/PartCoeff.csv'
-    pc_df = pd.read_csv(participation_coeff_path)
+        self.p2_peak = np.array([0.27])
+        self.p2_off_peak = np.array([0.09])
+        self.p2_peak_time = BASE['two_to_six']
+        self.p2_peak_season = SEASONS['summer']
 
-    pc_labels = pc_df.values[:, [0]]
-    pc_data = pc_df.values[:, [1]].astype(float)
+    def get_plans(self):
+        """
+        Using the input from the UI, switch probability for plan 1 and plan 2 are found (for each market segment)
+        :return: switch probability for each market segment
+        """
+        # import TN data
+        tn_data_path = Path(__file__).parent / '../data/tn_data.csv'
+        tn_df = pd.read_csv(tn_data_path)
 
-    # Plan one test with peak rate set as 2-5 summer only
-    p1_xu = np.exp(
-        BASE['peak'] * PLAN_ONE_PEAK
-        + BASE['off_peak'] * PLAN_ONE_OFF_PEAK
-        + BASE['two_to_five'] * np.array([1])
-        + SEASONS['summer'] * np.array([1])
-    )
+        # remove first column and headers from TN data
+        tn_df = tn_df.drop(tn_df.columns[[0]], axis=1)
+        tn_data = tn_df.values
 
-    # Plan two test with peak rate set as 2-6 summer only
-    p2_xu = np.exp(
-        BASE['peak'] * PLAN_TWO_PEAK
-        + BASE['off_peak'] * PLAN_TWO_OFF_PEAK
-        + BASE['two_to_six'] * np.array([1])
-        + SEASONS['summer'] * np.array([1])
-    )
+        # import participation coefficient data
+        participation_coefficients_path = Path(__file__).parent / '../data/participation_coefficients.csv'
+        pc_df = pd.read_csv(participation_coefficients_path)
 
-    # Hard-coded calculations
-    sum_xu = p1_xu + p2_xu
-    p1_prob = p1_xu / sum_xu
-    p2_prob = p2_xu / sum_xu
+        # separate participation coefficients into labels and data
+        pc_labels = pc_df.values[:, [0]]
+        pc_data = pc_df.values[:, [1]].astype(float)
 
-    scale_factor = np.array([0.3126567])
-    ex_scale_log_sum = np.exp(scale_factor * np.log(sum_xu))
-    part_rate = ex_scale_log_sum / (ex_scale_log_sum + np.exp(-1 * pc_data))
+        # calculate exponential utility for each plan
+        p1_xu = np.exp(
+            BASE['peak'] * self.p1_peak
+            + BASE['off_peak'] * self.p1_off_peak
+            + self.p1_peak_time * np.array([1])
+            + self.p1_peak_season * np.array([1])
+        )
+        p2_xu = np.exp(
+            BASE['peak'] * self.p2_peak
+            + BASE['off_peak'] * self.p2_off_peak
+            + self.p2_peak_time * np.array([1])
+            + self.p2_peak_season * np.array([1])
+        )
 
-    # Calculate plan rates by segment
-    p1_rates_by_segment = np.multiply(p1_prob, part_rate)
-    p2_rates_by_segment = np.multiply(p2_prob, part_rate)
+        # make calculations needed for logit model
+        sum_xu = p1_xu + p2_xu
+        p1_prob = p1_xu / sum_xu
+        p2_prob = p2_xu / sum_xu
 
-    # Give plan rates by segment titles
-    p1_rates_by_segment_column = ['plan 1 rate']
-    p2_rates_by_segment_column = ['plan 2 rate']
+        scale_factor = np.array([0.3126567])
+        ex_scale_log_sum = np.exp(scale_factor * np.log(sum_xu))
+        participation_rate = ex_scale_log_sum / (ex_scale_log_sum + np.exp(-1 * pc_data))
 
-    lp1_rate_by_segment = pd.DataFrame(p1_rates_by_segment, index=pc_labels, columns=p1_rates_by_segment_column)
-    lp2_rate_by_segment = pd.DataFrame(p2_rates_by_segment, index=pc_labels, columns=p2_rates_by_segment_column)
+        # calculate plan rates by segment
+        p1_rates_by_segment = np.multiply(p1_prob, participation_rate)
+        p2_rates_by_segment = np.multiply(p2_prob, participation_rate)
 
-    p1_num_by_segment_block = np.multiply(p1_rates_by_segment, tn_data)
-    p2_num_by_segment_block = np.multiply(p2_rates_by_segment, tn_data)
+        # Give plan rates by segment titles
+        p1_rates_by_segment_column = ['plan 1 rate']
+        p2_rates_by_segment_column = ['plan 2 rate']
 
-    p1_num_by_segment = p1_num_by_segment_block.sum(axis=1)
-    p2_num_by_segment = p2_num_by_segment_block.sum(axis=1)
+        lp1_rate_by_segment = pd.DataFrame(p1_rates_by_segment, index=pc_labels, columns=p1_rates_by_segment_column)
+        lp2_rate_by_segment = pd.DataFrame(p2_rates_by_segment, index=pc_labels, columns=p2_rates_by_segment_column)
 
-    p1_num_by_segment_columns = ['Plan 1 Number']
-    p2_num_by_segment_columns = ['Plan 2 Number']
-
-    lp1_num_by_segment = pd.DataFrame(p1_num_by_segment, index=pc_labels, columns=p1_num_by_segment_columns)
-    lp2_num_by_segment = pd.DataFrame(p2_num_by_segment, index=pc_labels, columns=p2_num_by_segment_columns)
-
-    print(lp1_num_by_segment)
-
-
-if __name__ == '__main__':
-    main()
+        return lp1_rate_by_segment, lp2_rate_by_segment
